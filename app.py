0257888a6ed2a6cdd550dbbb51d528031c959245
Robin <99472938+LoserPurp@users.noreply.github.com>
2024-01-14 19:58:38 +0100

    Version 2.2
    Works

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, flash, session
+from ldap3 import Server, Connection, SUBTREE, SIMPLE
+from ldap3 import SIMPLE, SUBTREE
+import json
+
+Author = "Robin Kleppe"
+Version = "2.2"
+
+#Loads config file
+with open('config.json', 'r') as file:
+    config = json.load(file)
+
+app = Flask(__name__)
+app.secret_key = config["secretKey"]  # Replace with a secure and random secret key
+
+def change_password(username, old_password, new_password):
+
+    try:
+        #Specify your AD server details
+        server = Server(config["serverAdress"], use_ssl=True)
+        #Connect to the AD server
+        with Connection(server, user=username + config["domain"], password=old_password, authentication=SIMPLE) as conn:
+            jes = conn.search(f'{config["searchGroup"]}',f'(&(sAMAccountName={username})'+str(config["searchUser"]), SUBTREE)
+            print(jes)
+
+            if len(conn.entries) == 1:
+                user_dn = conn.entries[0].entry_dn
+                #Change user password
+                response = conn.extend.microsoft.modify_password(user_dn, new_password, old_password=None)
+                print(f"Change status: {response}")
+                if response == True:
+                    return True
+            else:
+                return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        old_password = request.form.get('old_password')
+        new_password = request.form.get('new_password')
+        repeat_password = request.form.get('repeat_password')
+
+        if not username or not old_password or not new_password:
+            flash('All fields are required!')
+        elif new_password != repeat_password:
+            flash('Both password feelds must be the same')
+        elif change_password(username, old_password, new_password):
+            flash('Password changed successfully!')
+        else:
+            flash('Incorrect password or user is not available to change password. Contact the server admin if you believe this is an error!')
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
---
+++ static/script.js
@@ -0,0 +1,77 @@
+// Check password strength
+
+const newPasswordInput = document.getElementById('newPassword');
+const repeatPasswordInput = document.getElementById('repeatPassword');
+const submitButton = document.getElementById('submitButton');
+const passwordStrength = document.getElementById('passwordStrength');
+const strengthIndicator = document.getElementById('strengthIndicator');
+
+newPasswordInput.addEventListener('input', checkPassword);
+repeatPasswordInput.addEventListener('input', checkPassword);
+
+function checkPassword() {
+  const newPassword = newPasswordInput.value;
+
+  const strength = calculatePasswordStrength(newPassword);
+
+  updateStrengthIndicator(strength);
+
+  if (newPassword === repeatPassword && newPassword !== '' && repeatPassword !== '' && calculatePasswordStrength(password) == 'strong') {
+    submitButton.removeAttribute('disabled');
+    submitButton.classList.add('buttonChangeEnabled');
+  } else {
+    submitButton.setAttribute('disabled', 'disabled');
+    submitButton.classList.remove('buttonChangeEnabled');
+  }
+}
+
+function calculatePasswordStrength(password) {
+
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if (hasUppercase && hasLowercase && hasNumber && hasSpecialChar && password.length >= 8) {
+    return 'strong'; 
+  }
+  else if (hasUppercase && hasLowercase && hasNumber){
+    return 'high';
+  } 
+  else if (hasUppercase && hasLowercase){
+    return 'medium';
+  }
+  else {
+    return 'weak';
+  }
+}
+
+function updateStrengthIndicator(strength) {
+  strengthIndicator.className = '';
+  strengthIndicator.classList.add(strength);
+}
+
+
+
+
+// Toggle password visability
+
+var isPasswordVisible = false;
+
+function togglePassword(input) {
+  var passwordInput = document.getElementById(input);
+  var showPasswordIcon = document.getElementById(input + 'showPassword');
+  
+  isPasswordVisible = !isPasswordVisible;
+
+  if (isPasswordVisible) {
+    passwordInput.type = 'text';
+    showPasswordIcon.classList.remove('fa-eye');
+    showPasswordIcon.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    showPasswordIcon.classList.remove('fa-eye-slash');
+    showPasswordIcon.classList.add('fa-eye');
+  }
+}
+
