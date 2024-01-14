3b98e91aa4203a53b5ea0b069093bf7ed5bccd11
Robin <99472938+LoserPurp@users.noreply.github.com>
2024-01-14 23:02:30 +0100

    Changed to waitress

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=7234)
