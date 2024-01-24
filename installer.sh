b7c958af6bf1583cdbf8ced5aaf91a8df041bb76
Robin <99472938+LoserPurp@users.noreply.github.com>
2024-01-24 23:35:54 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,12 +2,16 @@
 
 #!!NOT TESTED!!
 
-#Install py modules
+#Installs py modules
 pip install -r requirements.txt
 
 #Creates the user and set its home directory
 sudo useradd -r -s /bin/false upc-user
 sudo chown upc-user:upc-user /var/www/user-password-changer
+
+#Gives read access to everyone
+sudo chmod -R 755 /var/www/user-password-changer
+
 
 #makes the systemd service file
 cat <<EOL | sudo tee /etc/systemd/system/user-password-changer.service > /dev/null
@@ -16,7 +20,7 @@ Description=User Password Changer Service
 
 [Service]
 Type=simple
-ExecStart=/var/www/user-password-changer/app.py
+ExecStart=/usr/bin/python3 /var/www/user-password-changer/app.py
 User=upc-user
 Group=upc-user
 Restart=always
@@ -25,10 +29,12 @@ Restart=always
 WantedBy=multi-user.target
 EOL
 
-#Grants access to the /var/www/user-password-changer directory
+#Grants access to the directory
 sudo chown -R upc-user:upc-user /var/www/user-password-changer
 
-#enable the service to start on boot
+#enables the service to start on boot
 sudo systemctl daemon-reload
 sudo systemctl enable user-password-changer
 sudo systemctl start user-password-changer
+
+echo("Thank you for using this project!")
